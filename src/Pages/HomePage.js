a71dc3e326b48ff45c65b67b8451a5504b168475
@sm202073 <sana.madhavan20@gmail.com>
2021-08-10 23:36:13 -0500

    cloned from template and started changing content

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,6 +4,8 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import GithubIcon from '@material-ui/icons/GitHub';
 import YoutubeIcon from '@material-ui/icons/YouTube';
 import Particle from '../Components/Particle';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 
 function HomePage() {
     return (
@@ -12,21 +14,23 @@ function HomePage() {
                 <Particle />
             </div>
             <div className="typography">
-                <h1>Hi, I'm <span>Lorem Ipsum</span></h1>
+                <h1>Hi, I'm <span>Sana Madhavan</span></h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Atque nihil voluptates ea dolore vel 
-                    repellat? Quia tenetur non quam exercitationem. Lorem ipsum dolor sit amet.
+                    A dedicated student aspiring to be a software engineer, social impact technologist, Natural
+                    Langauge Processing researcher, and future educator. Welcome to my corner of the Internet!
                 </p>
                 <div className="icons">
-                    <a href="https://codepen.io/pen/" className="icon i-facebook">
-                        <FacebookIcon />
+                    <a href="https://www.linkedin.com/in/sana-madhavan/" className="icon i-facebook">
+                        <LinkedInIcon />
                     </a>
-                    <a href="https://codepen.io/pen/" className="icon i-github">
+                    <a href="https://github.com/sm202073" className="icon i-github">
                         <GithubIcon />
                     </a>
+                    <a href="mailto:sana.madhavan20@gmail.com?subject=Hi Sana!" className="icon i-youtube">
+                        <EmailIcon />
+                    </a>
                     <a href="https://codepen.io/pen/" className="icon i-youtube">
-                        <YoutubeIcon />
+                        <FacebookIcon />
                     </a>
                 </div>
             </div>
