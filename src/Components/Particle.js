72a56e0b7fecec91709e04e2d8ab5aef8ab911bc
@sm202073 <sana.madhavan20@gmail.com>
2022-07-31 14:05:04 -0500

    updated particles

--- src/Components/Particle.js
+++ src/Components/Particle.js
@@ -5,66 +5,83 @@ import React from 'react';
 function Particle() {
   return (
     <>
-      <Particles width="100%" height="100vh" 
-              params={{
-                particles: {
-                  number:{
-                    value:109,
-                    density:{
-                      enable: true,
-                      value_area: 1200
-                    }
-                  },
-                  color:{
-                    value: '#636e72'
-                  },
-                  shape:{
-                    type: 'circle',
-                    stroke: {
-                      width: 0
-                    },
-                    polygon:{
-                      nb_sides: 6
-                    }
-                  },
-                  line_linked: {
-                    enable: false,
-                    distance: 150,
-                    opacity: 0.9,
-                    width: 1,
-                    shadow: {
-                      enable: true,
-                      color: "#e74c3c",
-                      blur: 5
-                    }
-                  },
-                  move:{
-                    enable: true,
-                    random: true,
-                    speed: .6,
-                    attract:{
-                      rotateX: 600,
-                      rotateY: 1200
-                    }
-                  },
-                  size: {
-                    value: 4,
-                    random: true,
-                    anim:{
-                      speed: 40,
-                      size_min: 0.1
-                    }
-                  },
-                  opacity: {
-                    value: 0.8,
-                    anim: {
-                      speed: 1,
-                      opacity_min: 0.1
-                    }
-                  }
-                }
-              }}
-      />
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0d47a1",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
     </>
   )
 }
